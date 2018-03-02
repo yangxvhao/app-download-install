f06042fee50f6820e0801cdb8625982c52e5288b
yangxuhao <xuhao.yang@cafintech.com>
2018-03-02 16:45:21 +0800

    优化

--- main/common_util.py
+++ main/common_util.py
@@ -1,34 +1,80 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-
+import requests
 import subprocess
+
+from requests.adapters import HTTPAdapter
+from pypinyin import lazy_pinyin
 
 __author__ = 'yangxvhao'
 
 
+def url_get(url):
+    try:
+        request = requests.Session()
+        # 设置超时重试次数为3
+        request.mount('http://', HTTPAdapter(max_retries=3))
+        request.mount('https://', HTTPAdapter(max_retries=3))
+        return request.get(url, timeout=3)
+    except requests.RequestException as e:
+        print('下载错误，请检查网络或稍后重试！')
+    except requests.URLRequired as e:
+        print('网站改版！')
+
+
 def read_file(path):
-    name_list = []
-    if os.path.exists(path):
-        f = open(path, 'r')
-        for line in f.readlines():
-            name_list.append(line.strip())
-        return name_list
-    else:
-        print('文件不存在')
+    try:
+        name_list = []
+        if os.path.exists(path):
+            f = open(path, 'r')
+            for line in f.readlines():
+                name_list.append(line.strip())
+            return name_list
+        else:
+            print('文件不存在')
+    except Exception as e:
+        print(e)
+
+
+def download_apk(path, apk_name, apk_url):
+    try:
+        apk_response = url_get(apk_url)
+        apk_file = os.path.join(path, ''.join(lazy_pinyin(apk_name)) + '.apk')
+
+        with open(apk_file, "wb") as code:
+            code.write(apk_response.content)
+
+        if os.path.exists(apk_file):
+            print('下载成功:' + apk_name)
+            return True
+    except Exception as e:
+        print(e)
 
 
 def install(path):
     not_install_list = []
-    filelist = os.listdir(path)
-    print('安装中.....')
-    for file in filelist:
-        filepath = os.path.join(path, file)
-        if os.path.exists(filepath):
-            status,output = subprocess.getstatusoutput('adb install ' + filepath)
-            if output.split('\n')[-1] != 'Success':
-                not_install_list.append(file + ',')
-    print('安装完成，安装失败的app: %s ' % not_install_list)
+    try:
+        file_list = os.listdir(path)
+        if len(file_list) == 0:
+            print('没有要安装的文件,请确认文件是否存在.')
+            exit(0)
+        print('安装中.....')
+        for file in file_list:
+            filepath = os.path.join(path, file)
+            if os.path.exists(filepath):
+                status, output = subprocess.getstatusoutput('adb install ' + filepath)
+                fail_str = output.split('\n')[-1]
+                if fail_str == 'Success':
+                    print('%s 安装成功' % file)
+                else:
+                    not_install_list.append('%s : %s,' % (file, fail_str))
+        if len(not_install_list) == 0:
+            print('安装完成!!!')
+        else:
+            print('安装失败的app: %s ' % not_install_list)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main':
--- main/download_install.py
+++ main/download_install.py
@@ -7,13 +7,11 @@ __author__ = 'yangxvhao'
 from bs4 import BeautifulSoup
 import json
 import os
-from pypinyin import lazy_pinyin
-import requests
 import random
 import common_util as common
 
 # apk安装包位置
-path = '/home/yangxvhao/apk/6.9'
+path = r'/home/yangxvhao/apk/new'
 
 # 要下载的apk名称所在文件
 apk_file_path = r'/home/yangxvhao/apk/6.8/app.txt'
@@ -21,10 +19,16 @@ apk_file_path = r'/home/yangxvhao/apk/6.8/app.txt'
 
 def downloads(*names):
     not_find_list = []
+    if len(os.listdir(path)) != 0:
+        print('下载文件夹必须为空文件夹！请新建文件夹或清空')
+        exit(0)
     for name in names:
         if not download(name):
             not_find_list.append(name)
-    print('未找到的app:' + str(not_find_list))
+    if len(not_find_list) == 0:
+        print('下载完成！')
+    else:
+        print('未找到的app:' + str(not_find_list))
 
 
 def download(name):
@@ -33,33 +37,25 @@ def download(name):
 
 def mi_download(name):
     try:
-        #域名地址
+        # 域名地址
         host = 'http://app.mi.com'
         root_url = 'http://app.mi.com/search?keywords='
         url = root_url + name
-        response = requests.get(url).content
+        response = common.url_get(url).content
         soup = BeautifulSoup(response, 'lxml')
         result_list = soup.select('ul.applist')[0].select('li')
         for result in result_list:
             app_name = result.h5.string
             if app_name == name:
                 # 查找下载地址
-                print('小米已找到app,下载中......')
+                print('小米已找到app,[%s] 下载中......' % app_name)
                 app_url = result.h5.a['href']
-                detail_response = requests.get(host + app_url).content
+                detail_response = common.url_get(host + app_url).content
                 detail_soup = BeautifulSoup(detail_response, 'lxml')
                 download_url = host + detail_soup.select('a.download')[0]['href']
 
                 # 下载apk文件
-                apk_response = requests.get(download_url)
-                apkfile = os.path.join(path, ''.join(lazy_pinyin(app_name)) + '.apk')
-
-                with open(apkfile, "wb") as code:
-                    code.write(apk_response.content)
-
-                if os.path.exists(apkfile):
-                    print('下载成功:' + app_name)
-                    return True
+                return common.download_apk(path, app_name, download_url)
     except Exception as e:
         print(str(e))
 
@@ -71,48 +67,38 @@ def sougou_download(name):
         # 地址
         root_url = 'http://as.sogou.com/so?w=1459&uID=lWDsPu4FxJN1QlVw&pid=34&query='
         # 查找apk下载地址
-        response = requests.get(root_url + name)
+        response = common.url_get(root_url + name)
         soup = BeautifulSoup(response.content, "lxml")
         result_list = soup.select('div.andriod_soft_item')
         for result in result_list:
             app_name = result.span.string
-            # 下载apk
             if app_name == name:
-                print('搜狗已找到app,下载中......')
+                print('搜狗已找到app,[%s] 下载中......' % app_name)
                 url_json_str = result.select('span.download_btn')[0].a['data-hd']
                 app_url = json.loads(url_json_str)['sogouHighdownUrl']
-                apk_response = requests.get(app_url)
-                apk_file = os.path.join(path, ''.join(lazy_pinyin(app_name)) + '.apk')
 
-                with open(apk_file, 'wb') as code:
-                    code.write(apk_response.content)
-
-                if os.path.exists(apk_file):
-                    print(app_name + '下载成功！')
-                    return True
+                # 下载apk文件
+                return common.download_apk(path, app_name, app_url)
     except Exception as e:
         print(str(e))
     return False
 
 
 def yingyongbao_download(name):
-    # 查找app下载地址
     try:
-        response = requests.get('http://mapp.qzone.qq.com/cgi-bin/mapp/mapp_search_result?keyword=%s&platform=touch&network_type=undefined&resolution=360x640&r=0.%s' % (name, str(random.randint(0, 9999999999999999))))
+        # 查找app下载地址
+        response = common.url_get('http://mapp.qzone.qq.com/cgi-bin/mapp/mapp_search_result?keyword=%s&platform='
+                                'touch&network_type=undefined&resolution=360x640&r=0.%s' %
+                                (name, str(random.randint(0, 9999999999999999))))
         apps = json.loads(str(response.content, encoding="utf-8").replace(';', ''))['apps']
         for app in apps:
             app_name = app['name']
             if app_name == name:
-                # 下载apk文件
-                print('应用宝已找到app,下载中......')
+
+                print('应用宝已找到app,[%s] 下载中......' % app_name)
                 app_url = app['url']
-                apk_file = os.path.join(path, ''.join(lazy_pinyin(app_name)) + '.apk')
-                apk_response = requests.get(app_url).content
-                with open(apk_file, 'wb') as file:
-                    file.write(apk_response)
-                if os.path.exists(apk_file):
-                    print(app_name + '下载成功！')
-                    return True
+                # 下载apk文件
+                return common.download_apk(path, app_name, app_url)
 
     except Exception as e:
         print(str(e))
@@ -120,6 +106,6 @@ def yingyongbao_download(name):
 
 
 if __name__ == '__main__':
-    # downloads(False, *common.read_file(apk_file_path))
-    downloads('友借','秒分')
+    downloads(*common.read_file(apk_file_path))
+    # downloads('友借')
     # common.install(path)
